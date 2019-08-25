c36a4164993aaf8eb7953ea7dab3cdf62b13fdc2
Igor Starostenko <igor.star@aol.com>
2019-08-24 20:16:43 -0700

    Updated site config

--- site-config.js
+++ site-config.js
@@ -1,16 +1,15 @@
 const path = require('path');
 
 module.exports = {
-  siteTitle: `Gatsby Universal`,
-  siteTitleShort: `GatsbyU`,
-  siteDescription: `An opinionated starter for Gatsby.`,
-  siteUrl: `https://gu.fabianschultz.com`,
-  themeColor: `#000`,
-  backgroundColor: `#fff`,
+  siteTitle: 'Igor Star Blog',
+  siteTitleShort: 'IgorStar',
+  siteDescription: 'Travel, photography, data and software.',
+  siteUrl: 'https://igorstar.com',
+  themeColor: '#000',
+  backgroundColor: '#fff',
   pathPrefix: null,
   logo: path.resolve(__dirname, 'src/images/icon.png'),
   social: {
-    twitter: `gatsbyjs`,
-    fbAppId: `966242223397117`,
+    twitter: 'IgorStarostenko',
   },
 };
