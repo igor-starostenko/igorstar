0e5764a34f527a8e214298cc0eddf258aea15db6
Igor Starostenko <igor.star@aol.com>
2020-01-30 00:51:05 -0800

    Added domain to s3 config

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,7 @@ module.exports = {
         secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
         bucketName: process.env.S3_BUCKET_NAME,
         region: process.env.S3_REGION,
+        domain: `s3-${process.env.S3_REGION}.amazonaws.com`,
       },
     },
     'gatsby-plugin-sharp',
