4b273ef7515400c48cf8c31b32729647b6cd7663
Igor Starostenko <igor.star@aol.com>
2020-01-29 22:22:28 -0800

    Added S3_ prefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,8 @@ const AWS = require('aws-sdk');
 const siteConfig = require('./site-config');
 
 AWS.config.update({
-  accessKeyId: process.env.ACCESS_KEY_ID,
-  secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  accessKeyId: process.env.S3_ACCESS_KEY_ID,
+  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
 });
 
 module.exports = {
