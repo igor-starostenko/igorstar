186bf58f2383c17f0e6761b9044373ec648bea80
Igor Starostenko <igor.star@aol.com>
2020-02-17 20:35:56 -0800

    Fixed contrast

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -1,9 +1,11 @@
 export const colors = {
-  accent: '#f90000',
+  accent: '#3453F5',
   primary: '#ffff00',
   secondary: '#0000ff',
+  darkGrey: '#3a3a3a',
   grey: '#757575',
   lightGrey: '#e5e5e5',
+  lightestGrey: '#f2f2f2',
   black: '#000000',
   white: '#ffffff',
 };
