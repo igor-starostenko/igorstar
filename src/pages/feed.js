341fb2d59c4abbc7ef64334820d125559390433f
Igor Starostenko <igor.star@aol.com>
2021-02-24 22:54:42 -0800

    Feed page added

---
+++ src/pages/feed.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
+import Layout from 'components/layout';
+import Box from 'components/box';
+import Gallery from 'components/gallery';
+import Head from 'components/head';
+import Title from 'components/title';
+
+const FeedPage = ({ data }) => (
+  <Layout>
+    <Head pageTitle={data.feedJson.title} />
+    <Box>
+      <Title as="h2" size="large">
+        {data.feedJson.title}
+      </Title>
+      <div style={{ margin: '0 -4rem' }}>
+        {data.images.edges.length > 0 && (
+          <Gallery
+            photos={data.images.edges}
+            order={data.feedJson.order}
+            targetRowHeight={250}
+          />
+        )}
+      </div>
+    </Box>
+  </Layout>
+);
+
+FeedPage.propTypes = {
+  data: PropTypes.shape({
+    feedJson: PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      order: PropTypes.string,
+    }).isRequired,
+    images: PropTypes.shape({
+      edges: PropTypes.arrayOf(
+        PropTypes.shape({
+          node: PropTypes.object.isRequired,
+        })
+      ).isRequired,
+    }).isRequired,
+  }).isRequired,
+};
+
+export default FeedPage;
+
+export const query = graphql`
+  query FeedQuery {
+    feedJson {
+      title
+      order
+    }
+    images: allS3ImageAsset(
+      sort: { fields: Key, order: DESC }
+      filter: { Key: { regex: "/^feed/.*$/" } }
+    ) {
+      edges {
+        image: node {
+          key: Key
+          childImageSharp {
+            original {
+              height
+              width
+            }
+            fluid(maxHeight: 1280, quality: 90) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+    }
+  }
+`;
