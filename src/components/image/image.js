6fddda9b2b8f1dcbd15ca5c18ed7ce135c2befc6
Igor Starostenko <igor.star@aol.com>
2021-10-09 23:46:41 -0700

    Image component updated

--- src/components/image/image.js
+++ src/components/image/image.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Img from 'gatsby-image';
+import Img from 'next/image';
 import { ImageWrapper } from './image.css';
 
-const Image = ({ style, fluid, alt, ...rest }) => (
+const Image = ({ style, src, alt, ...rest }) => (
   <ImageWrapper style={style} {...rest}>
-    <Img
-      fixed={typeof window === 'undefined' ? { src: {} } : undefined}
-      fluid={fluid}
-      alt={alt}
-    />
+    <Img src={src} alt={alt} />
   </ImageWrapper>
 );
 
 Image.propTypes = {
   style: PropTypes.object,
-  fluid: PropTypes.object.isRequired,
+  src: PropTypes.string.isRequired,
   alt: PropTypes.string.isRequired,
 };
 
