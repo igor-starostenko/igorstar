523c791a673bf026a0aead7bfa6f0f16d13a6900
Igor Starostenko <igor.star@aol.com>
2021-10-13 15:13:48 -0700

    get posts for home page from contentful

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+const contentful = require('contentful');
 // import { graphql } from 'gatsby';
 // import get from 'lodash/get';
 import Layout from 'components/layout';
 import Box from 'components/box';
 import Title from 'components/title';
-// import Article from 'components/article';
+import Article from 'components/article';
 
-const Index = () => (
+const Index = ({ posts }) => (
   <Layout>
     <Box>
       <div
@@ -25,41 +26,76 @@ const Index = () => (
           {data.homeJson.title}
           */}
         </Title>
-        <h4>0 Posts</h4>
-        {/*
-        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
-          */}
+        <h4>{posts.total} Posts</h4>
       </div>
       <div>
-        {/* {data.allMarkdownRemark.edges.map(({ node }, index) => (
+        {posts.items.map((post, index) => (
           <Article
-            key={node.id}
+            key={post.id}
             index={index}
-            image={get(
-              data.images.edges.find(edge =>
-                `/${edge.node.Key}`.includes(node.fields.slug)
-              ),
-              'node.image'
-            )}
-            slug={node.fields.slug}
-            title={node.frontmatter.title}
-            date={node.frontmatter.date}
-            description={node.frontmatter.description}
-            tags={node.frontmatter.tags}
-            captions={node.frontmatter.captions}
+            image={null}
+            slug={post.id}
+            title={post.title}
+            date={post.date}
+            description={post.description}
+            tags={post.tags}
           />
         ))}
-        */}
       </div>
     </Box>
   </Layout>
 );
 
 Index.propTypes = {
-  data: PropTypes.object.isRequired,
+  posts: PropTypes.shape({
+    limit: PropTypes.number.isRequired,
+    skip: PropTypes.number.isRequired,
+    total: PropTypes.number.isRequired,
+    items: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        createdAt: PropTypes.string,
+        updatedAt: PropTypes.string,
+        title: PropTypes.string.isRequired,
+        date: PropTypes.string.isRequired,
+        layout: PropTypes.string.isRequired,
+        draft: PropTypes.bool.isRequired,
+        category: PropTypes.string.isRequired,
+        tags: PropTypes.arrayOf(PropTypes.string),
+        description: PropTypes.string.isRequired,
+        content: PropTypes.object.isRequired,
+      }).isRequired
+    ),
+  }).isRequired,
 };
 
 export default Index;
+
+export const getStaticProps = async () => {
+  const limit = parseInt(process.env.CONTENTFUL_LIMIT || '100');
+  const client = contentful.createClient({
+    space: process.env.CONTENTFUL_SPACE,
+    accessToken: process.env.CONTENTFUL_TOKEN,
+  });
+
+  const response = await client.getEntries({ content_type: 'post', limit });
+
+  return {
+    props: {
+      posts: {
+        limit: response.limit,
+        skip: response.skip,
+        total: response.total,
+        items: response.items.map(({ sys, fields }) => ({
+          id: sys.id,
+          createdAt: sys.createdAt,
+          updatedAt: sys.updatedAt,
+          ...fields,
+        })),
+      },
+    },
+  };
+};
 
 // export const query = graphql`
 //   query HomeQuery {
