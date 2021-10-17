812426a172a985df495ccf8d23074c9a0ef0dd8d
Igor Starostenko <igor.star@aol.com>
2021-10-16 17:20:29 -0700

    Fixed selfie style

--- src/components/selfie/index.js
+++ src/components/selfie/index.js
@@ -1,29 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Box from 'components/box';
-import Image from 'next/image';
-// import Image from 'components/image';
+import { Image } from './selfie.css';
 
 const Selfie = ({ src, ...rest }) => (
   <Box style={{ padding: '2rem 4rem 0rem 4rem' }} {...rest}>
-    <Image
-      style={{
-        cursor: 'initial',
-        borderRadius: '50%',
-        border: '2px solid powderblue',
-        height: '100px',
-        width: '100px',
-      }}
-      height={100}
-      width={100}
-      src={src}
-      alt="Selfie"
-    />
+    <Image height={100} width={100} src={src} alt="Selfie" />
   </Box>
 );
 
 Selfie.propTypes = {
-  src: PropTypes.object.isRequired,
+  src: PropTypes.string.isRequired,
 };
 
 export default Selfie;
---
+++ src/components/selfie/selfie.css.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import Img from 'next/image';
+
+export const Image = styled(Img)`
+  cursor: initial;
+  border-radius: 50%;
+  border: 2px solid powderblue !important;
+`;
