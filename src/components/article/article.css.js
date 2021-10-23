451487a65ffb17e0d3048f1ca8c30a9887776675
Igor Starostenko <igor.star@aol.com>
2021-10-23 11:13:21 -0700

    Combined article and card components

--- src/components/article/article.css.js
+++ src/components/article/article.css.js
@@ -1,6 +1,9 @@
+import Link from 'next/link';
 import styled from 'styled-components';
+import { colors } from 'constants/theme';
+import MEDIA from 'helpers/mediaTemplates';
 
-export const Article = styled.article`
+export const Card = styled.article`
   &:first-child {
     margin-top: 19px;
   }
@@ -9,4 +12,56 @@ export const Article = styled.article`
     margin-bottom: 29px;
     border-bottom: 1px solid #e8e8e8;
   }
+`;
+
+export const SLink = styled(Link)`
+  text-decoration: none;
+`;
+
+export const Row = styled.div`
+  width: 100%;
+`;
+
+export const Title = styled.h3`
+  margin-bottom: 1rem;
+  color: ${colors.black};
+  display: inline-block;
+`;
+
+export const Description = styled.p`
+  text-decoration: none;
+  color: ${colors.black};
+`;
+
+export const Date = styled.span`
+  margin-bottom: 1rem;
+  margin-top: 2rem;
+  color: ${colors.grey};
+  float: right;
+  ${MEDIA.PHONE`
+    display: none;
+  `};
+`;
+
+export const MobileDate = styled.span`
+  display: none;
+  color: ${colors.grey};
+  float: right;
+  ${MEDIA.PHONE`
+    display: block;
+  `};
+`;
+
+export const Tags = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+export const Tag = styled.span`
+  margin: 0.5rem 10px 0.5rem 0px;
+  padding: 3px;
+  border-radius: 7px;
+  color: ${colors.darkGrey};
+  background: ${colors.lightestGrey};
+  font-size: 1.4rem;
 `;
--- src/components/article/index.js
+++ src/components/article/index.js
@@ -1,11 +1,94 @@
 import React from 'react';
-import Card from 'components/card';
-import { Article } from './article.css.js';
+import PropTypes from 'prop-types';
+import Link from 'next/link';
+import Image from 'components/image';
+import {
+  Card,
+  SLink,
+  Row,
+  Title,
+  Description,
+  Tags,
+  Tag,
+  Date as DateText,
+  MobileDate,
+} from './article.css.js';
 
-const Container = props => (
-  <Article>
-    <Card {...props} />
-  </Article>
-);
+const formatDate = dateString => {
+  const date = new Date(dateString);
+  return date.toLocaleDateString('en-us', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  });
+};
 
-export default Container;
+const Article = ({ index, path, title, image, date, description, tags }) => {
+  const href = `/posts/${path}`;
+
+  return (
+    <Card>
+      {image && (
+        <SLink href={href}>
+          <a>
+            <Image
+              style={{
+                margin: `${index === 0 ? 0 : 5.5}rem -3.9rem 0rem`,
+              }}
+              src={`https:${image.file.url}`}
+              alt={title}
+              {...image.file.details.image}
+            />
+          </a>
+        </SLink>
+      )}
+      <Row>
+        <SLink href={href}>
+          <a>
+            <Title>{title}</Title>
+          </a>
+        </SLink>
+        <DateText>— {formatDate(date)}</DateText>
+      </Row>
+      <Tags>
+        {tags.map(tag => (
+          <Tag key={tag}>#{tag}</Tag>
+        ))}
+      </Tags>
+      <Description>
+        {description}{' '}
+        <Link href={href}>
+          <a>Read more</a>
+        </Link>
+      </Description>
+      <MobileDate>{date}</MobileDate>
+    </Card>
+  );
+};
+
+Article.propTypes = {
+  index: PropTypes.number.isRequired,
+  path: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  image: PropTypes.shape({
+    file: PropTypes.shape({
+      url: PropTypes.string.isRequired,
+      details: PropTypes.shape({
+        image: PropTypes.shape({
+          width: PropTypes.number.isRequired,
+          height: PropTypes.number.isRequired,
+        }).isRequired,
+      }).isRequired,
+    }).isRequired,
+  }),
+  date: PropTypes.string.isRequired,
+  tags: PropTypes.arrayOf(PropTypes.string),
+};
+
+Article.defaultProps = {
+  tags: [],
+  image: null,
+};
+
+export default Article;
