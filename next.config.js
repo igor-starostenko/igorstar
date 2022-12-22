0260d07f315ce2fabf051ca9d662ef5c9f3749e5
Igor Starostenko <igor.star@aol.com>
2022-12-22 00:35:01 -0800

    Removed dotenv-webpack as it was conflicting with nextjs builtin implementation

--- next.config.js
+++ next.config.js
@@ -1,24 +1,7 @@
-const path = require('path');
-const Dotenv = require('dotenv-webpack');
-
 const next_config = {
   images: {
     domains: ['images.ctfassets.net'],
     ...{ ...(process.env.IMAGE_OPTIMIZATION ? {} : { loader: 'custom' }) },
-  },
-  webpack: (config) => {
-    config.plugins = config.plugins || [];
-
-    config.plugins = [
-      ...config.plugins,
-      // Read the .env file
-      new Dotenv({
-        path: path.join(__dirname, '.env'),
-        systemvars: true,
-      }),
-    ];
-
-    return config;
   },
 };
 
