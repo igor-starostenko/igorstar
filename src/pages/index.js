4f0cfbf53416eccab7578e09b58ee75b4e107db8
Igor Starostenko <igor.star@aol.com>
2022-12-24 16:05:28 -0800

    Added pagination for posts index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 import PropTypes from 'prop-types';
 import { getEntries, parseFields } from 'contentClient';
 import Layout from 'components/layout';
@@ -6,43 +7,64 @@ import Box from 'components/box';
 import Head from 'components/head';
 import Title from 'components/title';
 import Article from 'components/article';
+import Pagination from 'components/pagination';
 
-const Index = ({ page, posts }) => (
-  <Layout>
-    <Head pageTitle={page.title} />
-    <Box>
-      <div
-        style={{
-          display: 'inline-flex',
-          alignItems: 'baseline',
-          justifyItems: 'space-between',
-          width: '100%',
-          justifyContent: 'space-between',
-        }}
-      >
-        <Title as="h1" size="large">
-          {page.title}
-        </Title>
-        <span>{posts.total} Posts</span>
-      </div>
-      <div>
-        {posts.items.map((post, index) => (
-          <Article
-            key={post.id}
-            index={index}
-            image={post.thumbnail}
-            path={post.path}
-            title={post.title}
-            date={post.date}
-            description={post.description}
-            tags={post.tags}
-            linkText={post.linkText}
-          />
-        ))}
-      </div>
-    </Box>
-  </Layout>
-);
+const Index = ({ page, posts }) => {
+  const pageSize = 10;
+  const totalPages = Math.ceil(posts.total / pageSize);
+  const router = useRouter();
+  const pageNum = parseInt(router.query.page);
+  const displayCount = pageNum ? pageNum * pageSize : pageSize;
+
+  const displayPosts = posts.items.slice(
+    pageNum ? pageNum * pageSize - pageSize : 0,
+    displayCount
+  );
+
+  return (
+    <Layout>
+      <Head pageTitle={page.title} />
+      <Box>
+        <div
+          style={{
+            display: 'inline-flex',
+            alignItems: 'baseline',
+            justifyItems: 'space-between',
+            width: '100%',
+            justifyContent: 'space-between',
+          }}
+        >
+          <Title as="h1" size="large">
+            {page.title}
+          </Title>
+          <span>
+            Loaded {displayPosts.length} of {posts.total} posts
+          </span>
+        </div>
+        <div>
+          {displayPosts.map((post, index) => (
+            <Article
+              key={post.id}
+              index={index}
+              image={post.thumbnail}
+              path={post.path}
+              title={post.title}
+              date={post.date}
+              description={post.description}
+              tags={post.tags}
+              linkText={post.linkText}
+            />
+          ))}
+        </div>
+        {displayPosts.length < posts.total ? (
+          <Pagination pageNum={pageNum || 1} totalPages={totalPages} />
+        ) : (
+          ''
+        )}
+      </Box>
+    </Layout>
+  );
+};
 
 Index.propTypes = {
   page: PropTypes.shape({
