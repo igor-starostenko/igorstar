c746445e9da1bc674d812bdae69105aed0dac694
Igor Starostenko <igor.star@aol.com>
2023-01-08 21:48:37 -0800

    Script to export contentful assets

---
+++ scripts/contentful_assets.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const fetch = require('node-fetch');
+const contentful = require('contentful');
+require('dotenv').config();
+
+const imageDirectory = process.env.IMAGE_DIRECTORY || './public/images/';
+const limit = parseInt(process.env.CONTENTFUL_LIMIT || '100');
+
+const client = contentful.createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_DELIVERY_TOKEN,
+});
+
+const saveFile = async (i, url, name) => {
+  const response = await fetch(url);
+  const buffer = await response.buffer();
+  const path = imageDirectory + name;
+  fs.writeFile(path, buffer, () =>
+    console.log(`${i + 1}. Saved ${url} into ${path}`)
+  );
+};
+
+const getAllAssets = async (options) => {
+  let records = { items: [] };
+  // Ensure we load all records, regardless of the API limit
+  while (records.total != 0 && records.items.length < (records.total || 1)) {
+    let response = await client.getAssets({
+      ...options, // skip can't be overriden
+      skip: records.items.length,
+    });
+    records = { ...response, items: [...records.items, ...response.items] };
+  }
+  return records;
+};
+
+const saveAllAssets = async (items) => {
+  for (let i = 0; i < items.length; i += 1) {
+    const src = `https:${items[i].fields.file.url}`;
+    const fileName = `${items[i].sys.id}_${items[i].fields.file.fileName}`;
+    await saveFile(i, src, fileName);
+  }
+};
+
+(async () => {
+  const assets = await getAllAssets({ limit });
+  console.log(`Total assets: ${assets.total}`);
+  await saveAllAssets(assets.items);
+})();
