609941d2c41b9f4e07328138f8039833b78618fc
Hercules Agent <hercules@igorstar.com>
2026-05-31 01:40:13 +0000

    fix(next.config.js): use img loader for static export

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ import withBundleAnalyzer from '@next/bundle-analyzer';
 const next_config = {
   images: {
     unoptimized: true,
-    loader: 'custom',
+    loader: 'img',
     remotePatterns: [
       {
         protocol: 'https',
@@ -14,8 +14,6 @@ const next_config = {
         hostname: 'live.staticflickr.com',
       },
     ],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
   productionBrowserSourceMaps: true,
   turbopack: {},
