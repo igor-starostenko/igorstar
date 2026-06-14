e920fccca9e97335af22e89fc9f69ee5a999c9d3
Hercules Agent <hercules@igorstar.com>
2026-06-14 13:32:52 -0700

    Fixed about page selfie alignment and styling

--- src/components/selfie/selfie.jsx
+++ src/components/selfie/selfie.jsx
@@ -1,10 +1,20 @@
 import dynamic from 'next/dynamic';
 import PropTypes from 'prop-types';
+import { ImageWrapper } from './selfie.css.js';
 
-const Image = dynamic(() => import('components/image/image.jsx'));
+const NextImage = dynamic(() => import('next/image'));
 
 const Selfie = ({ src, ...rest }) => (
-  <Image height={100} width={100} src={src} alt="Selfie" priority {...rest} />
+  <ImageWrapper {...rest}>
+    <NextImage
+      height={100}
+      width={100}
+      src={src}
+      alt="Selfie"
+      priority
+      style={{ border: '2px solid powderblue !important', borderRadius: '50%' }}
+    />
+  </ImageWrapper>
 );
 
 Selfie.propTypes = {
