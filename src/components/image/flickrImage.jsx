15534c226246839a70b48f36272d9d9bf61d4202
Hercules Agent <hercules@igorstar.com>
2026-06-15 01:56:48 +0000

    fix: Add eslint disable for set-state-in-effect warning

--- src/components/image/flickrImage.jsx
+++ src/components/image/flickrImage.jsx
@@ -37,6 +37,7 @@ const FlickrImage = ({ xml, isRaw = false, backupSrc }) => {
           }
 
           if (href && title && src) {
+            // eslint-disable-next-line react-hooks/set-state-in-effect
             setData({ href, title, src, width, height });
           } else {
             setError(true);
