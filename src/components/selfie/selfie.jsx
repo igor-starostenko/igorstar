0eef07c96249109ff4f00f6b703e182c323f263d
Igor Starostenko <20246185+igor-starostenko@users.noreply.github.com>
2026-06-15 01:11:04 -0700

    Use BaseImage for selfie to properly pass props

--- src/components/selfie/selfie.jsx
+++ src/components/selfie/selfie.jsx
@@ -1,18 +1,17 @@
 import dynamic from 'next/dynamic';
 import PropTypes from 'prop-types';
-import { ImageWrapper } from './selfie.css.js';
+import { Image, ImageWrapper } from './selfie.css.js';
 
 const NextImage = dynamic(() => import('next/image'));
 
 const Selfie = ({ src, ...rest }) => (
   <ImageWrapper {...rest}>
-    <NextImage
+    <Image
       height={100}
       width={100}
       src={src}
       alt="Selfie"
       priority
-      style={{ border: '2px solid powderblue !important', borderRadius: '50%' }}
     />
   </ImageWrapper>
 );
