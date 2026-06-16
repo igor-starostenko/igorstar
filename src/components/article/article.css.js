2bc852404202dc1d2348e34ee5121db17785dde3
Igor Starostenko <20246185+igor-starostenko@users.noreply.github.com>
2026-06-16 00:50:06 -0700

    Fixed thumb height creating double frame

--- src/components/article/article.css.js
+++ src/components/article/article.css.js
@@ -47,15 +47,12 @@ export const Thumb = styled.div`
   }
 
   max-height: 41rem;
-  height: 60vw;
   margin: 0rem -3.9rem 0rem;
   ${MEDIA.TABLET`
     max-height: 36rem;
-    height: 55vw;
   `};
   ${MEDIA.PHONE`
     max-height: 24rem;
-    height: 50vw;
     margin: 0rem -2.9rem 0rem;
   `};
 `;
