0c207a39d8bf391e43ac5ca17190df0a6369f52b
Igor Starostenko <20246185+igor-starostenko@users.noreply.github.com>
2026-07-21 23:31:12 -0700

    Use custom loader

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ dotenv.config({ path: '.env', override: true });
 const next_config = {
   output: 'export',
   images: {
+    loader: 'custom',
     loaderFile: './src/helpers/contentfulLoader.js',
     remotePatterns: [
       {
