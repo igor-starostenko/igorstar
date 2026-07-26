c0b941eaf65b03a4d0de8c6085682057fbaa559d
Igor Starostenko <20246185+igor-starostenko@users.noreply.github.com>
2026-07-26 14:46:33 -0700

    Revert margin-left for nav container's first link to ensure it doesn't get too close to the logo

--- src/components/header/nav/nav.css.js
+++ src/components/header/nav/nav.css.js
@@ -4,14 +4,10 @@ export const Container = styled.nav`
   display: inline-flex;
 
   a {
+    margin-left: clamp(10px, 3vw, 25px);
     text-transform: uppercase;
     font-size: 1.4rem;
     line-height: 1.5;
     text-align: center;
-
-    /* Add space between items only, not before the first item */
-    &:not(:first-child) {
-      margin-left: clamp(10px, 3vw, 25px);
-    }
   }
 `;
