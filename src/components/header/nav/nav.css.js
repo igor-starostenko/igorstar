87ece5d5b3f906c800f583bdbe148cf722516a48
Hercules Agent <hercules@igorstar.com>
2026-07-26 21:40:45 +0000

    fix: address nav CSS issues
    - Remove invalid 'height: stretch' property
    - Fix margin-left to only apply between nav items (not before first)

--- src/components/header/nav/nav.css.js
+++ src/components/header/nav/nav.css.js
@@ -2,13 +2,16 @@ import styled from 'styled-components';
 
 export const Container = styled.nav`
   display: inline-flex;
-  height: stretch;
 
   a {
-    margin-left: clamp(10px, 3vw, 25px);
     text-transform: uppercase;
     font-size: 1.4rem;
     line-height: 1.5;
     text-align: center;
+
+    /* Add space between items only, not before the first item */
+    &:not(:first-child) {
+      margin-left: clamp(10px, 3vw, 25px);
+    }
   }
 `;
